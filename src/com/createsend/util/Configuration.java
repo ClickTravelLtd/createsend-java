8139d03750a6d0fe0faf568fb152a79ae31bae62
Simon <simon.mclean@clicktravel.com>
2011-11-14 13:35:47 +0000

    Revert "Got rid of stupid property loading in favour of good old fashioned, highly predictable system property loading…."
    This reverts commit 176011cfbe7e9d27e1aeac213898ce15f0c076b5.
    
    Signed-off-by: Simon <simon.mclean@clicktravel.com>

--- src/com/createsend/util/Configuration.java
+++ src/com/createsend/util/Configuration.java
@@ -1,50 +1,66 @@
 /**
  * Copyright (c) 2011 Toby Brain
  * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
- * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
- * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
- * Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
- * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
- * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
  */
-
 package com.createsend.util;
 
-
-
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class Configuration {
+    public static Configuration Current = new Configuration();
+        
+    private Properties properties;
+    private Configuration() {
+        properties = new Properties();
 
-	public static Configuration Current = new Configuration();
-
-
-	private Configuration() {
-	}
-
-
-	public String getApiKey() {
-		return System.getProperty("createsend.apikey");
-	}
-
-
-	public String getApiEndpoint() {
-		return System.getProperty("createsend.endpoint");
-	}
-
-
-	public String getWrapperVersion() {
-		return System.getProperty("createsend.version");
-	}
-
-
-	public boolean isLoggingEnabled() {
-		return Boolean.parseBoolean(System.getProperty("createsend.logging"));
-	}
+        try {
+            properties.load(getClass().getClassLoader().getResourceAsStream("com/createsend/util/config.properties"));
+                        
+            InputStream createsendProperties = getClass().getClassLoader().getResourceAsStream("createsend.properties");
+            if(createsendProperties != null) {
+                properties.load(createsendProperties);
+            }   
+        } catch (IOException e) {
+            e.printStackTrace();
+        }            
+    }
+    
+    public void addPropertiesFile(String filename) throws IOException {
+        properties.load(ClassLoader.getSystemResourceAsStream(filename));
+    }
+    
+    public String getApiKey() {
+        return properties.getProperty("createsend.apikey");
+    }
+    
+    public String getApiEndpoint() {
+        return properties.getProperty("createsend.endpoint");
+    }
+    
+    public String getWrapperVersion() {
+        return properties.getProperty("createsend.version");
+    }
+    
+    public boolean isLoggingEnabled() {
+        return Boolean.parseBoolean(properties.getProperty("createsend.logging"));
+    }
 }
